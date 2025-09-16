dd441e08da605d2bd9da29051bfdac793ca1e1d5
SchweizerWanderwege <92431739+SchweizerWanderwege@users.noreply.github.com>
2025-09-16 11:02:23 +0200

    Update Winterwanderwege_5_style.js

--- styles/Winterwanderwege_5_style.js
+++ styles/Winterwanderwege_5_style.js
@@ -63,6 +63,14 @@ var style_Winterwanderwege_5 = function(feature, resolution){
     return style;
 };
 
+function hexToRgba(hex, alpha) {
+    hex = hex.replace("#", "");
+    let r = parseInt(hex.substring(0, 2), 16);
+    let g = parseInt(hex.substring(2, 4), 16);
+    let b = parseInt(hex.substring(4, 6), 16);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
 function interactiveWinterStyle(feature, resolution) {
     // Pick resolution class
     let clazz;
